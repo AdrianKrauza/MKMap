3c32baf07e1db227a2568157d04e3e3c5ee136c4
AdrianKrauza <45629093+AdrianKrauza@users.noreply.github.com>
2020-07-18 13:23:30 +0200

    test

--- start.js
+++ start.js
@@ -15,7 +15,7 @@ const puppeteer = require('puppeteer');
       result
       await page.goto('https://map.kwadratowa.tv/#/-142/64/224/-3/Kwadratowa%20Masakra/normalrender', { waitUntil: 'networkidle0' });
       await page.click('#mcmap > div.leaflet-control-container > div.leaflet-top.leaflet-right > div:nth-child(4) > section > div.leaflet-control-layers-overlays > label:nth-child(1) > div > input'); 
-      await page.screenshot({path: `image/sezon4/${date.getMonth()}-${date.getDate()}-${date.getHours()}-${date.getMinutes()}.png`, clip: { x: 0, y: 0, width: 6700, height: 6700 }}  );
+      await page.screenshot({path: `${date.getMonth()}-${date.getDate()}-${date.getHours()}-${date.getMinutes()}.png`, clip: { x: 0, y: 0, width: 6700, height: 6700 }}  );
       await browser.close();
       console.log('success')
 })();
